70f5fb824847aef501eaac8b5c9bc6e63286e99d
Taylor Redding <taylorhredding@gmail.com>
2016-10-18 08:17:43 -0700

    Emoji Table & 2nd View Controller

--- emojiLibrary/ViewController.swift
+++ emojiLibrary/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tblView: UITableView!
+    
+    var emojis = ["😂", "😎", "👮", "🐹", "🐗", "👑", "🎅"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tblView.delegate = self
+        tblView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
